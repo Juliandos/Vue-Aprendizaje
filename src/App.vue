e1a75db7056e5e9d132835ba4a48636e65f68969
Julian Ortega <95juliandos@gmail.com>
2024-06-27 20:55:48 -0500

    directicvas js

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  const hola = 'Hola Mundo'
+  const styleColor = "color: blue"
+  const arrayColores = ["blue", "red", "violet"]
+  const activo = true
 </script>
-
+<!-- v-bind:  sirve para atributos o se reemplaza por : -->
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>
+    {{hola}}
+  </h1>
+  <h2 v-bind:style = "styleColor">Soy Azul</h2>
+  <h2>{{ arrayColores }}</h2>
+  <h2 :style = "`color: ${arrayColores[1]}`">ejemplo colores </h2>
+  <h2>{{ activo ? "activo" : "inactivo" }}</h2>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
